e21dd73d0c501182bd392b1482ab1111fb05c665
Vincent <pinxue.career@gmail.com>
2020-11-30 13:40:55 +0800

    conv2d v2 completed

--- conv2d_model.py
+++ conv2d_model.py
@@ -13,11 +13,11 @@ class MyConv2d(nn.modules.Conv2d):
 
         # Create Result Tensor
         result = torch.zeros(
-            [input.shape[0] * self.out_channels, width, height], dtype=torch.float32, device='cpu'
+            [input.shape[0] , self.out_channels, width, height], dtype=torch.float32, device='cuda'
         )
         #print("result size: ", result.shape)
         # Padding
-        input = F.pad(input, (self.padding[0], self.padding[0], self.padding[1], self.padding[1]))#.to('cuda')
+        input = F.pad(input, (self.padding[0], self.padding[0], self.padding[1], self.padding[1])).to('cuda')
 
         #print(input.shape)
         #print(result.shape)
@@ -28,24 +28,34 @@ class MyConv2d(nn.modules.Conv2d):
         #print("result sieze: ", result.shape)
         #print("stride: ", self.stride)
         print("Input Size: ", input.shape)
-        for n1 in range(input.shape[0]):
-            for n2 in range(self.out_channels):
-                for rr in range(result.shape[1]):
-                    for rc in range(result.shape[2]):
 
-                        r = rr*self.stride[1] ; c = rc*self.stride[0]
-                        result[n1*input.shape[0]+n2][rr][rc] = 0
-                        for ch in range(self.in_channels):
-                            result[n1*input.shape[0]+n2][rr][rc] += \
-                            input[n1][ch][r][c]*self.weight[n2][ch][0][0] + input[n1][ch][r][c+1]*self.weight[n2][ch][0][1] + input[n1][ch][r][c+2]*self.weight[n2][ch][0][2] + \
-                            input[n1][ch][r+1][c]*self.weight[n2][ch][1][0] + input[n1][ch][r+1][c+1]*self.weight[n2][ch][1][1] + input[n1][ch][r+1][c+2]*self.weight[n2][ch][1][2] + \
-                            input[n1][ch][r+2][c]*self.weight[n2][ch][2][0] + input[n1][ch][r+2][c+1]*self.weight[n2][ch][2][1] + input[n1][ch][r+2][c+2]*self.weight[n2][ch][2][2]
+        N = input.shape[0]
+        CH = input.shape[1]
+        H = input.shape[2]
+        W = input.shape[3]
+
+        C = self.weight.shape[0]
+        FR = self.weight.shape[2]
+        FC = self.weight.shape[3]
+
+        for n in range(N):
+            for row in range(0, H+1, self.stride[0]):
+                for col in range(0, W+1, self.stride[1]):
+                    for ch in range(CH):
+                        for i in range(C):
+                            for fr in range(FR):
+                                for fc in range(FC):
+                                    if( row+fr < H and col+fc < W):
+                                        result[n][i][fr][fc] +=     \
+                                        self.weight[i][ch][fr][fc] * \
+                                        input[n][ch][row+fr][col+fc]
+
+
+
         #print("Finished Conv2d Computation")
 
-        result = result.view(input.shape[0], self.out_channels, width, height)
+        #result = result.view(input.shape[0], self.out_channels, width, height)
         return result
-    def getWindows(self, input):
-        pass
 
     def calculateNewWidth(self, input):
         return (
@@ -93,10 +103,10 @@ class MyNet(nn.Module):
 
         #out = F.relu(out)
         return out
-'''
+
 #net = MyConv2d(3, 3, 3, padding=1)
-net = MyNet(3, 4)#.to('cuda')
-x = torch.randn(1, 3, 32, 32)#.to('cuda')
+net = MyNet(3, 4).to('cuda')
+x = torch.randn(1, 3, 32, 32).to('cuda')
 
 
 t5 = time.time()
@@ -106,4 +116,3 @@ print("\nInput Size: ", x.shape)
 print("\nOutput Size: ", out.shape)
 print('\nTotal Time Consumed: ' + str(round(t6-t5, 2)) + ' seconds')
 #print(out)
-'''
